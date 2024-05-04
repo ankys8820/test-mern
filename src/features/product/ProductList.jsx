5ec9cdf5f097e4f7d5e544b9971392479237d6e8
Ankit Shah <ankys8820@outlook.com>
2024-05-04 08:49:39 +0530

    Pagination added into ProductList

--- src/features/product/ProductList.jsx
+++ src/features/product/ProductList.jsx
@@ -10,6 +10,8 @@ import {
   MinusIcon,
   PlusIcon,
   Squares2X2Icon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
 } from "@heroicons/react/20/solid";
 
 const sortOptions = [
@@ -228,7 +230,7 @@ const ProductList = () => {
           <main className="mx-auto max-w-7xl px-4 sm:px-2 lg:px-8">
             <div className="flex items-baseline justify-between border-b border-gray-200 pb-2 pt-24">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-                All Prodocts
+                All Products
               </h1>
 
               <div className="flex items-center">
@@ -364,7 +366,7 @@ const ProductList = () => {
                   {/* Product list start*/}
                   <div>
                     <div className="bg-white">
-                      <div className="mx-auto max-w-2xl px-4 py-6 sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
+                      <div className="mx-auto max-w-2xl px-4 py-0 sm:px-0 sm:py-0 lg:max-w-7xl lg:px-8">
                         <div className=" grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                           {products.map((products) => (
                             <div key={products.id} className="group relative">
@@ -400,10 +402,81 @@ const ProductList = () => {
                       </div>
                     </div>
                   </div>
-                  {/* Product List End */}
                 </div>
+                {/* Product List End */}
               </div>
             </section>
+            {/* Section of products and filters end here */}
+            <>
+              {/* paginationa */}
+              <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+                <div className="flex flex-1 justify-between sm:hidden">
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    Previous
+                  </a>
+                  <a
+                    href="#"
+                    className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    Next
+                  </a>
+                </div>
+                <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                  <div>
+                    <p className="text-sm text-gray-700">
+                      Showing <span className="font-medium">1</span> to{" "}
+                      <span className="font-medium">10</span> of{" "}
+                      <span className="font-medium">97</span> results
+                    </p>
+                  </div>
+                  <div>
+                    <nav
+                      className="isolate inline-flex -space-x-px rounded-md shadow-sm"
+                      aria-label="Pagination"
+                    >
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        <span className="sr-only">Previous</span>
+                        <ChevronLeftIcon
+                          className="h-5 w-5"
+                          aria-hidden="true"
+                        />
+                      </a>
+                      {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
+                      <a
+                        href="#"
+                        aria-current="page"
+                        className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      >
+                        1
+                      </a>
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        2
+                      </a>
+
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        <span className="sr-only">Next</span>
+                        <ChevronRightIcon
+                          className="h-5 w-5"
+                          aria-hidden="true"
+                        />
+                      </a>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+            </>
           </main>
         </div>
       </div>
