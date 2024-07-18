66291a8305f6133e3c5b5ffe1a1df2eeed94020c
ankys8820 <ankys8820@outlook.com>
2024-07-18 17:39:56 +0530

    store rearrangement

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import  productReducer from '../features/product/productSlice';
+// import productReducer from "../features/product/productSlice";
 // import product from '../features/product/productApi';
 
-
 export const store = configureStore({
-    reducer: {
-        product: productReducer,
-    },
+  reducer: {
+    // product: productReducer,
+  },
 });
